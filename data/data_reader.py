7954cbb29fde71d490b44aace5170ef64e63c723
Sarthak Kumar <ksarthak962@gmail.com>
2026-04-30 10:19:45 -0400

    saved a profile

--- data/data_reader.py
+++ data/data_reader.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     with open('data/data.csv', mode='r') as file:
         reader = csv.DictReader(file)
         for row in reader:
-            plt.plot(float(row['radius'])/(6.897), float(row['mass'])/(1.989e30), 'o')
+            plt.plot(float(row['xi'])/(6.897), float(row['mass'])/(1.989e30), 'o')
             plt.xlabel('radius (solar radii)')
             plt.ylabel('mass (solar masses)')
             plt.savefig('data/profiles/mass_radius.png')
